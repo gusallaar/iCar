d80319753de5b6c8d74abc58cc8dfbf0a9ea91eb
Arezoo Allahyari <92451123+gusallaar@users.noreply.github.com>
2022-08-06 11:53:27 +0200

    Car throttle and basic movement
    On this section, I have implemented the basic movement of the car which can get different directional inputs and execute them. Also, I have initialized the IR sensor that can be used for the next stages.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,124 @@
+//This is mine
+
+#include <vector>
+
+#include <MQTT.h>
+#include <WiFi.h>
+#ifdef __SMCE__
+#include <OV767X.h>
+#endif
+
 #include <Smartcar.h>
 
+MQTTClient mqtt;
+WiFiClient net;
+
+const int fSpeed   = 50;  // 70% of the full speed forward
+const int bSpeed   = -50; // 70% of the full speed backward
+
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
+const auto oneSecond = 1000UL;
+#ifdef __SMCE__
+const auto triggerPin = 6;
+const auto echoPin = 7;
+const auto mqttBrokerUrl = "127.0.0.1";
+#else
+const auto triggerPin = 33;
+const auto echoPin = 32;
+const auto mqttBrokerUrl = "192.168.0.40";
+#endif
+const auto maxDistance = 400;
+
+const int FRONT_IR_PIN = 0;
+const int LEFT_IR_PIN  = 1;
+const int RIGHT_IR_PIN = 2;
+const int BACK_IR_PIN  = 3;
+
+// These are the initiallazation of the accurare IR sensor
+GP2Y0A21 frontIR(arduinoRuntime, FRONT_IR_PIN);
+GP2Y0A21 leftIR(arduinoRuntime, LEFT_IR_PIN);
+GP2Y0A21 rightIR(arduinoRuntime, RIGHT_IR_PIN);
+GP2Y0A21 backIR(arduinoRuntime,BACK_IR_PIN);
+
+// This is the initiallazation of the gyroscope
+SR04 front(arduinoRuntime, triggerPin, echoPin, maxDistance);
+
+std::vector<char> frameBuffer;
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+#ifdef __SMCE__
+  Camera.begin(QVGA, RGB888, 15);
+  frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
+#endif
+
+  WiFi.begin(ssid, pass);
+  mqtt.begin(mqttBrokerUrl, 1883, net);
+
+  Serial.println("Connecting to WiFi...");
+  auto wifiStatus = WiFi.status();
+  while (wifiStatus != WL_CONNECTED && wifiStatus != WL_NO_SHIELD) {
+    Serial.println(wifiStatus);
+    Serial.print(".");
+    delay(1000);
+    wifiStatus = WiFi.status();
+  }
+
+
+  Serial.println("Connecting to MQTT broker");
+  while (!mqtt.connect("arduino", "public", "public")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  mqtt.subscribe("/smartcar/control/#", 1);
+  mqtt.onMessage([](String topic, String message) {
+    if (topic == "/smartcar/control/throttle") {
+      car.setSpeed(message.toInt());
+    } else if (topic == "/smartcar/control/steering") {
+      car.setAngle(message.toInt());
+    } else {
+      Serial.println(topic + " " + message);
+    }
+  });
 }
 
+/////////////////////////////////////////////////////////////////////////////////
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  if (mqtt.connected()) {
+    mqtt.loop();
+    const auto currentTime = millis();
+#ifdef __SMCE__
+    static auto previousFrame = 0UL;
+    if (currentTime - previousFrame >= 50) {
+      previousFrame = currentTime;
+      Camera.readFrame(frameBuffer.data());
+      mqtt.publish("/smartcar/camera", frameBuffer.data(), frameBuffer.size(),
+                   false, 0);
+    }
+#endif
+    static auto previousTransmission = 0UL;
+    if (currentTime - previousTransmission >= oneSecond) {
+      previousTransmission = currentTime;
+      const auto distance = String(front.getDistance());
+      mqtt.publish("/smartcar/ultrasound/front", distance);
+    }
+  }
+#ifdef __SMCE__
+  // This delay is for avoiding the CPU time-over
+  delay(1);
+#endif
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////
